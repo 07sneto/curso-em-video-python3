abd4929c55b0608351a7cb436a9fd01fda62c913
Severino Neto <severinoneto591@gmail.com>
2026-08-18 23:48:45 -0300

    Add files via upload

---
+++ mundo02/02 - Condições em Python if elif/aula012.py
@@ -0,0 +1,13 @@
+nome = str(input('Qual é seu nome? '))
+
+# Isto é uma Condição Aninhada
+if nome == 'Neto':
+    print('Que nome bonito!')
+elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
+    print('Seu nome é bem popular no Brasil.')
+elif nome in 'Ana Claudia Jessica Juliana':
+    print('Belo nome feminino.')
+else:
+    print('Seu nome é bem normal.')
+
+print('Tenha um bom dia, {}!'.format(nome))
